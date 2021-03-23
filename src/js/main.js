95f87d8c6294ef42e23a24e43a84c31976734981
diegocastilho <diego.ueg@gmail.com>
2021-03-23 16:29:20 -0300

    Validação do formulário com jquery, js e regex

--- src/js/main.js
+++ src/js/main.js
@@ -60,4 +60,48 @@ $(document).ready(function() {
 
     const arrowR = document.querySelector(".owl-next");
     arrowR.innerHTML =  '<div class="arrow-right"><svg width="20" height="18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.25 7.75h14.825L11.538 2.3A1.251 1.251 0 1113.463.7l6.25 7.5c.041.06.08.122.112.187 0 .063 0 .1.088.163.056.143.086.296.087.45a1.25 1.25 0 01-.087.45c0 .062 0 .1-.088.162-.033.066-.07.128-.113.188l-6.25 7.5a1.248 1.248 0 01-.962.45 1.25 1.25 0 01-.963-2.05l4.538-5.45H1.25a1.25 1.25 0 110-2.5z" fill="#fff"/></svg></div>';
+
+
 })
+
+// Email validation
+let emailInput;
+
+$("#email-input").on("change", function() {
+    emailInput = $(this).val();
+    if (validateEmail(emailInput)) {
+        $(this).css({
+            color: "green",
+            border: "1px solid green",
+        });
+    } else {
+        $(this).css({
+            color: "red",
+            border: "1px solid red"
+        });
+    }
+});
+
+$("#subscribe-button").on("click", function(e) {
+    e.preventDefault();
+    if (validateEmail(emailInput)) {
+        document.getElementById("email-input").value = "";
+        document.getElementById("email-input").placeholder = "Your email has been registered.";
+
+    } else {
+        document.getElementById("email-input").value = "";
+        document.getElementById("email-input").placeholder = "Enter a valid email.";
+        return false;
+    }
+});
+
+
+// Validation email regex
+function validateEmail(email) {
+    var pattern = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+    return $.trim(email).match(pattern) ? true : false;
+}
+
+
+
